453f4666357e87620ff7f16bc0881b851db3415a
Sata lesmana <lesmanasata@gmail.com>
2021-11-24 23:18:56 +0700

    login app

--- src/app/page/auth/login/login.component.ts
+++ src/app/page/auth/login/login.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl
+} from '@angular/forms';
+import { AuthService } from '../../../service/auth/auth.service'
+import { Router } from '@angular/router';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -6,12 +13,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  password:string;
-  constructor() { 
-    this.password = ''
+  loginForm: FormGroup
+  constructor(
+    private service: AuthService,
+    private router: Router,
+
+  ) {
+    this.loginForm = this.loginFormFormGroup()
+  }
+
+  loginFormFormGroup() {
+    return new FormGroup({
+      email: new FormControl(),
+      password: new FormControl()
+    })
   }
 
   ngOnInit(): void {
+  }
+
+  onLogin() {
+    this.service.sendLogin(this.loginForm.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate(['/']);
+        },
+        error => {
+          alert(error?.error?.msg)
+        });
   }
 
 }
--- src/app/service/auth/auth.service.ts
+++ src/app/service/auth/auth.service.ts
@@ -1,28 +1,47 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { observable, Observable, throwError } from "rxjs";
+import { BehaviorSubject, observable, Observable, throwError } from "rxjs";
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   public baseUrl = 'https://app-apotik-server.herokuapp.com'
-  constructor(private http: HttpClient) { }
+  private userSubject: BehaviorSubject<any>;
 
-  public sendRegister(data:any):Observable<any> {
+  constructor(private http: HttpClient) {
+    this.userSubject = new BehaviorSubject<any>(null);
+  }
+
+  public sendRegister(data: any): Observable<any> {
     const payload = new HttpParams()
       .set('name', data.name)
       .set('email', data.email)
       .set('phone', data.phone)
       .set('password', data.password);
 
-      return new Observable<any>(observer => {
-        this.http.post(this.baseUrl + '/api/auth/register', payload)
-          .subscribe((respond: any) => {
-            observer.next(respond);
-            observer.complete();
-            return { unsubcribe() { respond } };
-          });
-      });
+    return new Observable<any>(observer => {
+      this.http.post(this.baseUrl + '/api/auth/register', payload)
+        .subscribe((respond: any) => {
+          observer.next(respond);
+          observer.complete();
+          return { unsubcribe() { respond } };
+        });
+    });
   }
+
+  public sendLogin(data: any): Observable<any> {
+    const payload = new HttpParams()
+      .set('email', data.email)
+      .set('password', data.password);
+
+    return this.http.post(this.baseUrl + '/api/auth/login', payload)
+      .pipe(map(auth => {
+        localStorage.setItem('auth', JSON.stringify(auth));
+        this.userSubject.next(auth);
+        return auth;
+      }));
+  }
+
 }
